79069d0d1d6722455862c72f9e039e5c19f524ed
Pablo <cpablop1@gmail.com>
2024-12-22 12:46:15 -0600

    If else en Angular

--- angular/learning-angular/src/app/home/home.component.ts
+++ angular/learning-angular/src/app/home/home.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  public identified: boolean;
+
+  constructor() {
+    this.identified = false
+  }
+
+  login(): void{
+    this.identified = true;
+  }
+
+  logout(): void{
+    this.identified = false;
+  }
 
 }
