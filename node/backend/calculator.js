60d0426e43fe993838f0d302fd1b2babc4613a35
Pablo <cpablop1@gmail.com>
2024-12-24 10:01:35 -0600

    Reviewing Node Js

---
+++ node/backend/calculator.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var number1 = 10;
+var number2 = 6;
+
+var template = `
+The sum is: ${number1 + number2}
+The remainder is: ${number1 - number2}
+The multiplication is: ${number1 * number2}
+The division is: ${number1 / number2}
+`;
+
+
+console.log(template);
