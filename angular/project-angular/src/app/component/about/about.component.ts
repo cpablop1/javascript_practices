6a318f2fe13c20ad7f02f00b19f02a5d68ecaa1e
Pablo <cpablop1@gmail.com>
2024-12-25 16:39:56 -0600

    About section sytles

--- angular/project-angular/src/app/component/about/about.component.ts
+++ angular/project-angular/src/app/component/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public email: string;
 
+  // Constructor method
+  constructor() {
+    this.title = 'Cruz Pablo Pérez';
+    this.subtitle = 'Software Developer';
+    this.email = 'cpablop1@gmail.com';
+  }
+
+  ngOnInit(): void {
+    // Code here....
+  }
 }
